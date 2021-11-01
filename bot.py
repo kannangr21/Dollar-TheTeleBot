0d14573d59cb32bcaa6fa7b856c9f50ca7c2c97b
kannangr21 <kannanravi2105@gmail.com>
2021-11-02 02:26:43 +0530

    Updated return messages

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ def help(message):
         "`ExpNumber`: Expense number.(used to delete expense)\n"
         "\n*Contact my developer:*\n"
         "[Kannan G R](https://t.me/kannangr21)"
-), parse_mode = "MarkDown")
+        ), parse_mode = "MarkDown")
 
 # -------------- /newplan ------------------------
 
@@ -88,7 +88,7 @@ def newplan(message):
         msg = msg[1].split(' -')
     except IndexError:
         return bot.reply_to(message, ("Please use the format,"
-        "\n`/newplan {plan name} -c {keyword for the plan (2 letters recommended)} -d {optional description} -a {Amount}`"), 
+        "\n`/newplan {Plan name} -c {Plan code} -d {Optional description} -a {budget}`"), 
         parse_mode = "MarkDown")
     newPlan = msg.pop(0)
     desc, code, amt = None, "", 0
@@ -110,18 +110,18 @@ def newplan(message):
                 amt = float(element.split("a",1)[1])
     if amt == 0 or code == "":
         return bot.reply_to(message, (
-        "Estimated Amount not added! Please use -a flag to add amount."
+        "*Format not recognized*"
         "\nTry using,"
-        "\n```/newplan {Plan name} -c {keyword for the plan (2 letters recommended)} -d {optional description} -a {amount in numbers}```"
-        "\n/// Remember to add spaces before flags ' -d' and ' -a'///"
-        ))
+        "\n`/newplan {Plan name} -c {Plan code} -d {Optional description} -a {budget}`"
+        ),parse_mode = "MarkDown")
     plans = users_collection.find_one({"_id" : message.from_user.id},{"_id" : 0, "plans" : 1})
     plans = plans["plans"]
     for plan in plans:
         if plan["code"] == code:
             return bot.reply_to(message, (
-                f"Hey! It seems like you've already added `{code}` as the short code for {plan['plan']}.\n"
-                "Try adding another code for the same plan!"))
+                f"Hey! It seems like you've already added *{code}* as the short code for *{plan['plan']}*.\n"
+                "Try adding another code for the new plan!"),
+                parse_mode = "MarkDown")
     data = {
         "code": code, 
         "plan": newPlan,
@@ -131,7 +131,7 @@ def newplan(message):
         "expenses" : []
         }
     users_collection.update_one({"_id": message.from_user.id},{"$push":{"plans":data}})
-    return bot.reply_to(message,f"{newPlan} - Plan added successfully with an estimated expense of {amt}")
+    return bot.reply_to(message,f"*{newPlan}* - Plan added successfully with an estimated expense of *{amt}*",parse_mode = "MarkDown")
 
 # -------------- /viewplans ------------------------
 
@@ -141,12 +141,12 @@ def getPlans(message):
     plans = plans["plans"]
     if plans == []:
         return bot.reply_to(message, "No plans added yet!. Try /newplan to create a plan!")
-    retmsg = "Your plans are \n"
+    retmsg = "Your plans are,\n  *Plans* - *Codes*\n"
     i = 1
     for plan in plans:
         retmsg += f"{i}. {plan['plan']} - {plan['code']}\n"
         i += 1
-    return bot.reply_to(message, retmsg)
+    return bot.reply_to(message, retmsg, parse_mode="MarkDown")
 
 # -------------- /plan ------------------------
 
@@ -155,18 +155,19 @@ def getOnePlan(message):
     try:
         code = message.text.split("/plan ")[1]
     except:
-        return bot.reply_to(message, "Use /plan {code} to view about the plan.\nUse /viewplans to get the codes.")
+        return bot.reply_to(message, "Use `/plan {Plan code}` to view about a plan.\nUse /viewplans to get the codes.", 
+        parse_mode = "MarkDown")
     plans = users_collection.find_one({"_id" : message.from_user.id},{"_id" : 0, "plans" : 1})
     plans = plans["plans"]
     for plan in plans:
         if plan["code"] == code:
             desc = "" 
             if plan['desc'] != None:
-                desc = f"Description - {plan['desc']}\n"
-            retMsg = (f"Code - {plan['code']}\n"
-                    f"Plan - {plan['plan']}\n"
+                desc = f"*Description* - {plan['desc']}\n"
+            retMsg = (f"*Code* - {plan['code']}\n"
+                    f"*Plan* - {plan['plan']}\n"
                     +desc+
-                    f"Estimated amount - {plan['amt']}\n"
+                    f"*Estimated amount* - {plan['amt']}\n"
                     "Expenses under this plan : ")
             i = 1  
             for exp in plan["expenses"]:
@@ -175,8 +176,9 @@ def getOnePlan(message):
             if i == 1:
                 retMsg += "\nNo amount has been spent for this plan.\nUse /expplan to add expenses on this plan."
             else:
-                retMsg += f"\nAmount spent - {plan['spent']}"
-            return bot.reply_to(message, retMsg)
+                retMsg += f"\n*Amount spent* - {plan['spent']}"
+                retMsg += f"\n*Balance* - {plan['amt']-plan['spent']}"
+            return bot.reply_to(message, retMsg, parse_mode = "MarkDown")
     return bot.reply_to(message, "No such event is found!\nTry /viewplans to get the code or use /newplan to create a plan.")
 
 # -------------- /expplan ------------------------
@@ -186,7 +188,8 @@ def addExpPlan(message):
     try:
         msg = message.text.split("/expplan ")[1]
     except:
-        return bot.reply_to(message, "Hey there, To add an expense use \n`/expplan {plan code} -e {expense} -a {amount}`")
+        return bot.reply_to(message, "Hey there, To add an expense use \n`/expplan {Plan code} -e {expense} -a {amount}`", 
+        parse_mode = "MarkDown")
     msg = msg.split(" -")
     code = msg.pop(0)
     plans = users_collection.find_one({"_id" : message.from_user.id},{"_id" : 0, "plans" : 1})
@@ -199,14 +202,23 @@ def addExpPlan(message):
                     try:
                         exp = data.split("e ",1)[1]
                     except IndexError:
-                        return bot.reply_to(message, "Found some error in the syntax,  Try using,\n`/expplan {code} -e {expense title} -a {amount}")        
+                        return bot.reply_to(message, ("*Format not recognized*"
+                                                "\nTry using,"
+                                                "\n`/expplan {Plan code} -e {expense} -a {amount}`"),
+                                                parse_mode = "MarkDown")        
                 if data[0] == "a":
                     try:
                         amt = data.split("a ",1)[1]
                     except IndexError:
-                        return bot.reply_to(message, "Found some error in the syntax,  Try using,\n`/expplan {code} -e {expense title} -a {amount}")
+                        return bot.reply_to(message, ("*Format not recognized*"
+                                                "\nTry using,"
+                                                "\n`/expplan {Plan code} -e {expense} -a {amount}`"), 
+                                                parse_mode = "MarkDown")
             if amt == 0 or exp == "":
-                return bot.reply_to(message, "Found some error in the syntax,  Try using,\n`/expplan {code} -e {expense title} -a {amount}")
+                return bot.reply_to(message, ("*Format not recognized*"
+                                                "\nTry using,"
+                                                "\n`/expplan {Plan code} -e {expense} -a {amount}`"), 
+                                                parse_mode = "MarkDown")
             expense = {
                 "desc" : exp,
                 "amt" : amt
@@ -217,7 +229,8 @@ def addExpPlan(message):
             return bot.reply_to(message, "Expenses updated!")
 
     else:
-        return bot.reply_to(message, f"No plan named as {code} is found!\nUse /viewplans to know the codes.\nAlso, use /expplan to know the format.")            
+        return bot.reply_to(message, (f"No plan named as *{code}* found!"
+                                    "\nUse /viewplans to know the codes."), parse_mode = "MarkDown")            
     
 
 
@@ -228,7 +241,8 @@ def deletePlan(message):
     try:
         code = message.text.split("/delplan ")[1]
     except:
-        return bot.reply_to(message, f"Hey {message.from_user.first_name}, Try using /delplan "+"{shortcode} to delete a plan!")
+        return bot.reply_to(message, (f"Hey {message.from_user.first_name},\n"
+        "Try using `/delplan {Plan code}` to delete a plan!"), parse_mode="MarkDown")
     plans = users_collection.find_one({"_id" : message.from_user.id},{"_id" : 0, "plans" : 1})
     plans, i = plans["plans"], 0
     for plan in plans:
@@ -237,14 +251,14 @@ def deletePlan(message):
             users_collection.update_one({"_id": message.from_user.id},{"$set":{"plans":plans}})
             desc = ""
             if plan['desc'] != None:
-                desc = f"Description - {plan['desc']}\n"
-            retMsg = (f"Code - {plan['code']}\n"
-                    f"Plan - {plan['plan']}\n"
+                desc = f"*Description* - {plan['desc']}\n"
+            retMsg = (f"*Code* - {plan['code']}\n"
+                    f"*Plan* - {plan['plan']}\n"
                     +desc+
-                    f"Estimated amount - {plan['amt']}\n"
-                    f"Amount spent - {plan['spent']}\n"
+                    f"*Estimated amount* - {plan['amt']}\n"
+                    f"*Amount spent* - {plan['spent']}\n"
                     "\nThe above plan has been deleted successfully.")        
-            return bot.reply_to(message, retMsg)
+            return bot.reply_to(message, retMsg, parse_mode = "MarkDown")
         i += 1
     return bot.reply_to(message, "No such plan found!\nTry /viewplans to get your plans.")
 
@@ -253,26 +267,30 @@ def deletePlan(message):
 @bot.message_handler(commands = ["exp"])
 def addExpense(message):
     if message.text == "/exp":
-        return bot.reply_to(message, "To add an expense try the format,\n`/exp {detail} -a {amount}`\n"
-                                        "To add an expense to the existing plan, try `/expplan`")
+        return bot.reply_to(message, ("To add an expense try the format,\n`/exp {expense} -a {amount}`\n"
+                                      "To add an expense to the existing plan, try `/expplan`"), 
+                                       parse_mode = "MarkDown")
     try:
         msg = message.text.split("/exp ")[1]
     except:
-        return bot.reply_to(message, (f"Hello {message.from_user.first_name}!\nTo add an expense try the format,\n`/exp"+" {detail} -a {amount}`\n"
-                                        "To add an expense to the existing plan, try `/expplan`"))
+        return bot.reply_to(message, (f"Hello {message.from_user.first_name}!\n"
+                                    "To add an expense try the format,\n`/exp {expense} -a {amount}`\n"
+                                    "To add an expense to the existing plan, try /expplan"), parse_mode = "MarkDown")
     try:
         msg = msg.split(" -a ")
         desc = msg[0]
         amt = float(msg[1])
     except:
-        return bot.reply_to(message, (f"Hello {message.from_user.first_name}!\nTo add an expense, try the format,\n`/exp"
-                                       " {detail} -a {amount}`\n"))
+        return bot.reply_to(message, (f"Hello {message.from_user.first_name}!\n"
+                                    "To add an expense, try the format,\n"
+                                    "`/exp {expense} -a {amount}`"),parse_mode = "MarkDown")
     data = {
         "desc" : desc,
         "amt" : amt
     }
     users_collection.update_one({"_id": message.from_user.id}, {"$push":{"expenses": data}})
-    return bot.reply_to(message, f"Your expense {desc} has been added.\nAnd the expense is {amt}")
+    return bot.reply_to(message, f"Your expense *{desc}* has been added.\nAnd the amount is *{amt}*",
+                       parse_mode = "MarkDown")
 
 # -------------- /viewexp ------------------------
 
@@ -281,15 +299,16 @@ def getExpenses(message):
     expenses = users_collection.find_one({"_id" : message.from_user.id},{"_id" : 0,"expenses" : 1})
     expenses = expenses["expenses"]
     if expenses == []:
-        return bot.reply_to(message, f"Hey {message.from_user.first_name}, You haven't added any expense. Use `/exp` to add an expense that is not in a plan.")
+        return bot.reply_to(message, (f"Hey {message.from_user.first_name},\n"
+        "You haven't added any expense. Use /exp to add an expense that is not in a plan."))
     retMsg = "Your expenses are,\n"
     total, i = 0, 1
     for expense in expenses:
         total += expense['amt']
         retMsg += (f"{i}. {expense['desc']} - {str(expense['amt'])}\n")
         i += 1
-    retMsg += f"\nTotal = {total}"
-    return bot.reply_to(message, retMsg)
+    retMsg += f"\n*Total : {total}*"
+    return bot.reply_to(message, retMsg, parse_mode = "MarkDown")
 
 # -------------- /delexp ------------------------
 
@@ -298,13 +317,16 @@ def delExpense(message):
     try:
         num = message.text.split("/delexp ")[1]
     except:
-        return bot.reply_to(message, "Try using the format\n `/delexp {expense number}`\nUse /viewexp to know the expense number.")
+        return bot.reply_to(message, ("Try using the format,"
+                            "\n`/delexp {expense number}`\n"
+                            "Use /viewexp to know the expense number."),
+                            parse_mode = "MarkDown")
     user_exp = users_collection.find_one({"_id": message.from_user.id},{"_id":0,"expenses": 1})
     user_exp = user_exp["expenses"]
     try:
         exp = user_exp.pop(int(num) - 1)
         users_collection.update_one({"_id":message.from_user.id},{"$set":{"expenses": user_exp}})
-        return bot.reply_to(message, f"{exp['desc']} Has been removed from the list")
+        return bot.reply_to(message, f"*{exp['desc']}* has been removed from the list", parse_mode = "MarkDown")
     except:
         return bot.reply_to(message, "No expense found!\nTry using /viewexp to get the expense number.")
  
